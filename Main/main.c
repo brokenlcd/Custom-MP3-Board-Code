a6b78918ac2497ec642955f14b8b8a04e36a45c3
brokenlcd <brokenlcd@gmail.com>
2010-01-05 23:03:06 -0700

    Starting pretty much from scratch, I'm gonna do it my way.

--- Main/main.c
+++ Main/main.c
@@ -36,4 +36,49 @@ static void timer0ISR(void);
 static void timer1ISR(void);
 
 
-// and now I start from scratch.
+// time to start from scratch
+
+/*
+first function referenced in the now broken build is delay_ms
+so the first function I will define is delay_ms.
+the clock is at 12 MHz, multiplied by 5 to get 60 MHz for the
+microprocessor speed. This means an instruction may be executed
+every 1x1e-8 seconds. To get a millisecond delay, we multiply by
+1000. So we get an instruction every 1x1e-6 milliseconds. This
+means that to delay for a millisecond, we need to execute 1000000
+nops in that time.
+*/
+
+/*
+ * function: void delay_ms(int count)
+ *      input: the number of milliseconds to delay for
+ *      output: no output but the system will pause for n milliseconds
+ */
+
+void delay_ms(int count) {
+    int i = 0;
+    count *= 1000000;
+    for (i; i < count; i++) { asm("nop"); }
+    return;
+}
+
+
+// a quick reset function
+
+void reset(void) {
+    // by triggering a fault in the watchdog, the MCU will be reset
+    WDMOD |= 3;
+    WDFEED = 0xAA;
+    WDFEED = 0x55;
+    WDFEED = 0xAA;
+    WDFEED = 0x00;
+}
+
+/****************************************
+ *          INTERNAL VARS               *
+ ****************************************/
+
+/****************************************
+ *          EXTERNAL VARS               *
+ ****************************************/
+
